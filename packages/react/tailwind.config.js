faeb943286cf6edc239001dd9b6ddd055b34945e
Gomah <hi@gomah.fr>
2022-09-27 20:42:34 +1000

    feat(tailwind-preset): accepts a function to override tokens

--- packages/react/tailwind.config.js
+++ packages/react/tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 
-const tokens = require('@acme/tokens');
-
 module.exports = {
   content: ['./src/**/*.{js,ts,css,jsx,tsx}'],
   presets: [
-    require('@acme/tailwind-preset')
+    require('@acme/tailwind-preset')()
   ]
 };
--- packages/tailwind-preset/src/index.ts
+++ packages/tailwind-preset/src/index.ts
@@ -1,22 +1,27 @@
-const tokens = require('@acme/tokens');
+const defaultTokens = require('@acme/tokens');
 
-module.exports = {
-  theme: {
-    extend: {
-      ringWidth: {
-        '1.5': '1.5px',
-      },
-      colors: {
-        ...tokens.colors,
-      },
-      boxShadow: {
-        ...tokens.shadows,
+module.exports = function AcmeTailwindPreset({ tokens = defaultTokens } = {}) {
+  // TODO: Merge tokens with defaultTokens
+  return {
+    theme: {
+      extend: {
+        ringWidth: {
+          '1.5': '1.5px',
+        },
+        colors: {
+          ...tokens.colors,
+        },
+        boxShadow: {
+          ...tokens.shadows,
+        }
       }
-    }
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
-};
+    },
+    plugins: [
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/typography'),
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+}
+
+
