c215c5830292ec3f6debbf1034a7f5c483d1b87b
Gomah <hi@gomah.fr>
2022-10-09 20:08:07 +1100

    feat(react): remove @uiball/loaders dependency

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx';
 import * as React from 'react';
 import { twMerge } from 'tailwind-merge';
-import { Ring } from '@uiball/loaders';
 
 export interface ButtonProps extends React.ComponentPropsWithRef<'button'> {
   /**
@@ -42,6 +41,8 @@ export interface ButtonProps extends React.ComponentPropsWithRef<'button'> {
    * Full width?
    */
   fullWidth?: boolean;
+
+  loadingSpinner?: React.ReactNode;
 }
 
 const styles = /*tw*/ {
@@ -95,6 +96,7 @@ export const Button = React.forwardRef<HTMLButtonElement, React.PropsWithChildre
       loading,
       trailingIcon,
       fullWidth,
+      loadingSpinner,
       colour = 'primary',
       rounded,
       ...props
@@ -106,7 +108,8 @@ export const Button = React.forwardRef<HTMLButtonElement, React.PropsWithChildre
         styles.colour[colour],
         styles.size[size],
         rounded ? styles.rounded.full : styles.rounded.default,
-        !!leadingIcon || !!trailingIcon ? 'inline-flex items-center space-x-2' : '',
+        (!!leadingIcon || !!trailingIcon) && 'inline-flex items-center justify-center space-x-2',
+        loading && 'loading',
         styles.shadow,
         styles.focus,
         styles.border,
@@ -120,8 +123,30 @@ export const Button = React.forwardRef<HTMLButtonElement, React.PropsWithChildre
     return (
       <button ref={ref} {...props} className={buttonClasses}>
         {loading ? (
-          <Ring size={20} lineWeight={4} speed={2} color="currentColor" />
-        ) : leadingIcon || trailingIcon ? (
+          loadingSpinner || (
+            <svg
+              className="h-5 w-5 animate-spin text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          )
+        ) :
+        leadingIcon || trailingIcon ? (
           <>
             {leadingIcon && <span>{leadingIcon}</span>}
             <span>{children}</span>
