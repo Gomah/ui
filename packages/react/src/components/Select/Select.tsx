e77594b8b9b088433d20aa677d6f829300f6f167
Gomah <hi@gomah.fr>
2022-10-11 21:13:53 +1100

    feat(react): Select styles

--- packages/react/src/components/Select/Select.tsx
+++ packages/react/src/components/Select/Select.tsx
@@ -14,7 +14,16 @@ export interface SelectProps<T> extends Omit<React.ComponentPropsWithRef<'div'>,
   disabled?: boolean;
   multiple?: boolean;
   onChange?: (option: T) => void;
+
+  size?: keyof typeof styles.size;
 }
+
+const styles = /*tw*/ {
+  size: {
+    sm: 'py-2 px-3 text-sm',
+    md: 'py-2.5 px-3.5 ',
+  },
+};
 
 function _select<T>(
   {
@@ -27,6 +36,7 @@ function _select<T>(
     selected,
     placeholder = 'Select',
     className,
+    size = 'md',
     ...props
   }: SelectProps<T>,
   ref: React.ForwardedRef<HTMLDivElement>
@@ -40,14 +50,30 @@ function _select<T>(
 
   const buttonClassNames = twMerge(
     clsx(
-      'relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-primary-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-primary-300 sm:text-sm'
+      styles.size[size],
+      'relative w-full bg-white cursor-default rounded-lg shadow-xs border border-gray-300 transition',
+      'text-left placeholder-gray-100',
+      '',
+      'focus:outline-none focus-visible:ring-4 focus-visible:ring-primary-100'
     ),
     className
   );
 
   const optionsClassNames = clsx(
-    'absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+    'absolute mt-[5px] max-h-60 w-full overflow-auto rounded-md bg-white py-1 shadow-lg ring-1 ring-gray-200 focus:outline-none'
   );
+
+  const optionContainerClassNames = (active: boolean) =>
+    clsx(
+      'relative cursor-default select-none py-0.5 px-1.5',
+      // 'relative cursor-default select-none py-2 pl-10 pr-4',
+      // active ? 'bg-primary-100 text-primary-900' : 'text-gray-900'
+    );
+
+  const optionClassNames = ({ selected, active, disabled}: Record<string, boolean>) =>
+    clsx('pb-2.5 pl-2 pr-2.5 pt-2.5 space-x-2',
+      (selected || active) && 'bg-gray-50 rounded-md'
+    );
 
   return (
     <Listbox
@@ -59,57 +85,57 @@ function _select<T>(
       disabled={disabled}
       onChange={onChange}
     >
-      <div className="relative mt-1">
-        <Listbox.Button className={buttonClassNames}>
-          {/* @ts-ignore */}
-          <span className="block truncate">
-            {!!displayKey
-              ? // @ts-ignore
-                selectedOption?.[displayKey]
-              : // @ts-ignore
-                selectedOption?.join(', ') || placeholder}
-          </span>
-          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-            <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-          </span>
-        </Listbox.Button>
-        <Transition
-          as={React.Fragment}
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Listbox.Options className={optionsClassNames}>
-            {options.map((option: T) => (
-              <Listbox.Option
-                // @ts-ignore
-                key={by ? option[by] : option}
-                value={option}
-                className={({ active }) =>
-                  clsx(
-                    'relative cursor-default select-none py-2 pl-10 pr-4',
-                    active ? 'bg-primary-100 text-primary-900' : 'text-gray-900'
-                  )
-                }
-              >
-                {({ selected }) => (
-                  <>
-                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
-                      {/* @ts-ignore */}
-                      {displayKey ? option[displayKey] : option}
-                    </span>
-                    {selected ? (
-                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-primary-600">
-                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+      {({ open }) => (
+        <div className="relative mt-1">
+          <Listbox.Button className={clsx(buttonClassNames, open && 'ring-4 ring-primary-100')}>
+            <span className="block truncate">
+              {!!displayKey
+                ? // @ts-ignore
+                  selectedOption?.[displayKey]
+                : // @ts-ignore
+                  selectedOption?.join(', ') || (
+                    <span className="text-gray-500">{placeholder}</span>
+                  )}
+            </span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={React.Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className={optionsClassNames}>
+              {options.map((option: T) => (
+                <Listbox.Option
+                  // @ts-ignore
+                  key={by ? option[by] : option}
+                  value={option}
+                  className={({ active }) => optionContainerClassNames(active)}
+                >
+                  {({ selected, active, disabled }) => (
+                    <div className={optionClassNames({ selected, active, disabled})}>
+                      <span
+                        className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}
+                      >
+                        {/* @ts-ignore */}
+                        {displayKey ? option[displayKey] : option}
                       </span>
-                    ) : null}
-                  </>
-                )}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </Transition>
-      </div>
+                      {selected ? (
+                        <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-primary-600">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </div>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      )}
     </Listbox>
   );
 }
