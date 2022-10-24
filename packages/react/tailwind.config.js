6f9815745557694cb90d7195b17f87f34a12212c
Gomah <hi@gomah.fr>
2022-10-25 08:42:15 +1100

    chore: quick cleanup

--- packages/react/tailwind.config.js
+++ packages/react/tailwind.config.js
@@ -5,7 +5,6 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./src/**/*.{js,ts,css,jsx,tsx}'],
   theme: {
-    // TODO: Only have this for ladle
     fontFamily: {
       sans: ['Inter var', ...defaultTheme.fontFamily.sans],
     },
