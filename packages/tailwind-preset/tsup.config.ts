a5ff92d71c2ac2d64810d1da3ad9e258199f7f69
Gomah <hi@gomah.fr>
2022-11-10 11:56:09 +1100

    fix(tailwind-preset): adjust tsup build

--- packages/tailwind-preset/tsup.config.ts
+++ packages/tailwind-preset/tsup.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  splitting: true,
   dts: true,
   format: ['cjs', 'esm'],
-  treeshake: true,
   sourcemap: false,
   clean: true,
 });
