888c38de5cd64ae3703838ebe859cae0a2877f4f
Gomah <hi@gomah.fr>
2022-11-11 14:17:49 +1100

    feat(react): add Link recipe

--- packages/react/src/components/Link/Link.tsx
+++ packages/react/src/components/Link/Link.tsx
@@ -1,37 +1,12 @@
 import * as React from 'react';
-import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { link, LinkVariants } from './Link.css';
 
-const styles = /*tw*/ {
-  size: {
-    xs: 'text-xs',
-    sm: 'text-sm',
-    md: 'text-base',
-    lg: 'text-lg',
-    xl: 'text-xl',
-  },
-  weight: {
-    regular: 'font-normal',
-    medium: 'font-medium',
-    semibold: 'font-semibold',
-    bold: 'font-bold',
-  },
-};
-
-export type LinkSize = keyof typeof styles.size;
-export type LinkWeight = keyof typeof styles.weight;
-
-export interface LinkProps extends React.ComponentPropsWithRef<'a'> {
-  size?: LinkSize;
-  weight?: LinkWeight;
-}
+export type LinkProps = React.ComponentPropsWithRef<'a'> & LinkVariants;
 
 export const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
   ({ size = 'md', weight = 'regular', className, children, ...props }, ref) => {
-    const textClass = twMerge(
-      clsx('text-primary-600', styles.size[size], styles.weight[weight]),
-      className
-    );
+    const textClass = twMerge(link({ size, weight }), className);
 
     return (
       <a ref={ref} {...props} className={textClass}>
