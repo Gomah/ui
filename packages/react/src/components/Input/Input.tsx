c63d2e177ff4ced92b39950883f379d9992a008d
Gomah <hi@gomah.fr>
2022-11-11 14:18:06 +1100

    fix(react): adjust InputProps

--- packages/react/src/components/Input/Input.tsx
+++ packages/react/src/components/Input/Input.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { twMerge } from 'tailwind-merge';
 import { input, InputVariants } from './Input.css';
 
-export type InputProps = {}  & Omit<React.ComponentPropsWithRef<'input'>, 'size'> & InputVariants
+export type InputProps = Omit<React.ComponentPropsWithRef<'input'>, 'size'> & InputVariants;
 
 export const Input = React.forwardRef<HTMLInputElement, React.PropsWithRef<InputProps>>(
   ({ children, className, intent = 'gray', type = 'text', size = 'md', ...props }, ref) => {
