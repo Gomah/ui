4da61a7bbe28783c1d730b857497804e53f66da8
Gomah <hi@gomah.fr>
2022-12-01 11:49:37 +1100

    feat(react): add intent variant to Tag

--- packages/react/src/components/Tag/Tag.css.ts
+++ packages/react/src/components/Tag/Tag.css.ts
@@ -1,13 +1,22 @@
 import { recipe, RecipeVariants } from '@vanilla-extract/recipes';
 
 export const tag = recipe({
-  base: ['text-gray-700', 'rounded-md font-medium border border-gray-300'],
+  base: ['rounded-md', 'font-medium', 'border'],
   variants: {
+    intent: {
+      gray: 'text-gray-700 border-gray-300',
+      primary: 'text-primary-700 border-primary-300',
+    },
     size: {
       sm: 'text-sm py-[3px] px-2',
       md: 'text-base py-0.5 px-[9px]',
       lg: 'text-base py-1 px-2.5',
     },
+  },
+
+  defaultVariants: {
+    size: 'md',
+    intent: 'gray',
   },
 });
 
